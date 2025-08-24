400df33f187f718a290251ed16f09fdbe8e0dfb7
Richard Ngasike <107096899+richardngasike@users.noreply.github.com>
2025-08-24 08:36:21 +0300

    Add files via upload

--- src/app/components/WhatsAppFAB.js
+++ src/app/components/WhatsAppFAB.js
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect } from "react";
-import styles from "./WhatsAppFAB.module.css";
+import styles from "../styles/WhatsAppFAB.module.css";
 
 const WhatsAppFAB = () => {
   useEffect(() => {
